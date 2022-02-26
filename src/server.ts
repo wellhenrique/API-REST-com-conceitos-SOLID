f67c1ba7a16449bff171359c669c07c9eb7c310b
wellhenri <welissonh80@gmail.com>
2022-02-26 20:34:53 -0300

    refatorando-as-rotas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import express from "express";
+import { router } from "./routes";
 import { categoriesRouter } from "./routes/categories.routes";
 import { specificationsRoutes } from "./routes/specifications.routes";
 
 const app = express();
-
 app.use(express.json());
-
-app.use("/categories", categoriesRouter);
-
-app.use("/specifications", specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
