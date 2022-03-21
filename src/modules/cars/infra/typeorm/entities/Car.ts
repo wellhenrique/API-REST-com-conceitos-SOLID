65b8abd5966c219f49942f907124546923d2e398
wellhenri <welissonh80@gmail.com>
2022-03-21 12:23:26 -0300

    iniciando TDD Cars

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,23 @@
+class Car {
+  id: string;
+
+  name: string;
+
+  description: string;
+
+  dailY_rate: number;
+
+  avaliable: boolean;
+
+  license_plate: string;
+
+  fine_amount: number;
+
+  brand: string;
+
+  category_id: string;
+
+  created_at: Date;
+}
+
+export { Car };
